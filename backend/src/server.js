333f91c0512d87078f5f2d48d7e8e1ecd428aced
taikhoanchuafile <vunguyen.vn700@gmail.com>
2025-10-28 11:23:33 +0700

    chuan bi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,22 +3,32 @@ import taskRoute from "./routes/tasksRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 connectDB();
 dotenv.config(); // load biến từ file .env
+
 const PORT = process.env.PORT || 5000;
 
 //Midleware
 app.use(express.json());
-app.use(
-  cors({
-    // origin: ["https://myapp.com"],
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-    // credentials: true,
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use("/api/tasks", taskRoute);
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Khoi tao server cong 5001 thanh cong");
